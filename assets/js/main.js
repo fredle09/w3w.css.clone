21e808dbb360252b22393b15cf3fcd19b80cbebd
FredLe09 <le197903@gmail.com>
2022-09-09 23:21:09 +0700

    Update modal Turn On/Off

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,14 @@
 // For Button buy tickers
 const buyBtns = document.querySelectorAll(".js-buy-ticker");
+const modal = document.querySelector(".js-modal");
+const modalClose = document.querySelector(".js-modal-close");
 
 for (const buyBtn of buyBtns) {
   buyBtn.addEventListener("click", () => {
-    alert("Show");
+    modal.classList.add("open");
+  });
+
+  modalClose.addEventListener("click", () => {
+    modal.classList.remove("open");
   });
 }
