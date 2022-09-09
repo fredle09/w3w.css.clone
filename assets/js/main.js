44c938a9a15cb6dac5e20857ba5582247e1ced5e
FredLe09 <le197903@gmail.com>
2022-09-09 21:08:27 +0700

    Upload Code

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// For Button buy tickers
+const buyBtns = document.querySelectorAll(".js-buy-ticker");
+
+for (const buyBtn of buyBtns) {
+  buyBtn.addEventListener("click", () => {
+    alert("Show");
+  });
+}
