33bc6daed0278b13f90666c0c93692386387b3a7
FredLe09 <le197903@gmail.com>
2022-09-09 23:35:21 +0700

    Update Close Container

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,28 @@
 // For Button buy tickers
 const buyBtns = document.querySelectorAll(".js-buy-ticker");
 const modal = document.querySelector(".js-modal");
+const modalContainer = document.querySelector(".js-modal-container");
 const modalCloses = document.querySelectorAll(".js-modal-btn-close");
 
+// Open the modal when the user clicks the buy-ticker button
 for (const buyBtn of buyBtns) {
   buyBtn.addEventListener("click", () => {
     modal.classList.add("open");
   });
 
+  // Close the modal when the user clicks on the close-btn
   for (const modalClose of modalCloses) {
     modalClose.addEventListener("click", () => {
       modal.classList.remove("open");
     });
   }
+
+  // Close the modal when the user clicks outside of the modal-container
+  modal.addEventListener("click", () => {
+    modal.classList.remove("open");
+  });
+
+  modalContainer.addEventListener("click", (event) => {
+    event.stopPropagation();
+  });
 }
